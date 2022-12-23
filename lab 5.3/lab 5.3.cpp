ac2d23e0133edc209b24394926a673331a7dbf51
popofffnik <57560494+popofffnik@users.noreply.github.com>
2022-12-23 19:44:20 +0300

    Добавьте файлы проекта.

---
+++ lab 5.3/lab 5.3.cpp
@@ -0,0 +1,290 @@
+﻿#include <iostream>
+
+
+
+int main()
+
+{
+
+	//WeaponClass sword("Sword",10,2,ONEHANDED); 
+
+	MagicWeaponClass staff("FirstStaff", 10, 2, TWOHANDED, 3);
+
+	staff.Attack();
+
+
+
+	cout << "\n\n";
+
+
+
+	SingleUsedWeapon knife("FirstKnife", 5, 1, ONEHANDED, false);
+
+	knife.Attack();
+
+
+
+	cout << "\n\n";
+
+
+
+	knife.Attack();
+
+
+
+	cout << "\n\n";
+
+
+
+	WeaponHand<bool> someWeapon(0, 1);
+
+	cout << someWeapon.getIsLeft() << " " << someWeapon.getIsRight() << endl;
+
+	WeaponHand<string> anotherWeapon("Is lefthand", "Is righthand");
+
+	cout << anotherWeapon.getIsLeft() << " " << anotherWeapon.getIsRight() << endl;
+
+}
+
+====
+
+WeaponClass.h
+
+#pragma once
+
+#include <iostream>
+
+using namespace std;
+
+enum WeaponType
+
+{
+
+	ONEHANDED,
+
+	TWOHANDED,
+
+	BOW,
+
+	CROSSBOW
+
+};
+
+
+
+class WeaponClass
+
+{
+
+public:
+
+	string name;
+
+	int damage;
+
+	int weight;
+
+	WeaponType weaponType;
+
+
+
+	WeaponClass(string name, int damage, int weight, WeaponType weaponType) :
+
+		name(name), damage(damage), weight(weight), weaponType(weaponType) {}
+
+
+
+	//WeaponClass() { "DefaulName"; 10; 2; weaponType = ONEHANDED; }
+
+
+
+	WeaponType getType()
+
+	{
+
+		return weaponType;
+
+	}
+
+
+
+
+
+	virtual int getDamage()
+
+	{
+
+		return this->damage;
+
+	}
+
+
+
+
+
+	bool operator > (WeaponClass& otherWeapon)
+
+	{
+
+		return this->getDamage() > otherWeapon.getDamage();
+
+	}
+
+	bool operator < (WeaponClass& otherWeapon)
+
+	{
+
+		return this->getDamage() < otherWeapon.getDamage();
+
+	}
+
+
+
+	virtual void Attack() = 0;
+
+};
+
+
+
+
+
+
+
+class MagicWeaponClass :public WeaponClass
+
+{
+
+	int additionalDamage;
+
+public:
+
+	MagicWeaponClass(string name, int damage, int weight, WeaponType weaponType, int additionalDamage) :
+
+		WeaponClass(name, damage, weight, weaponType), additionalDamage(additionalDamage) {}
+
+
+
+	//MagicWeaponClass() { "DefaultName"; 10; 2; 0; 1; }
+
+
+
+	int getAddDamage()
+
+	{
+
+		return additionalDamage;
+
+	};
+
+	int getDamage() override
+
+	{
+
+		return WeaponClass::getDamage() + getAddDamage();
+
+	}
+
+
+
+	void Attack() override
+
+	{
+
+		cout << "Magic weapon attacks";
+
+	}
+
+
+
+};
+
+class SingleUsedWeapon :public WeaponClass
+
+{
+
+	bool isUsed;
+
+public:
+
+	SingleUsedWeapon(string name, int damage, int weight, WeaponType weaponType, bool isUsed) :
+
+		WeaponClass(name, damage, weight, weaponType), isUsed(isUsed) {}
+
+	void Attack() override
+
+	{
+
+		if (!isUsed)
+
+		{
+
+			cout << "Weapon attacks";
+
+			isUsed = true;
+
+		}
+
+		else
+
+		{
+
+			cout << "Weapon already used";
+
+		}
+
+
+
+	}
+
+};
+
+
+
+template <typename T>
+
+class WeaponHand
+
+{
+
+	T isLeft;
+
+	T isRight;
+
+public:
+
+	WeaponHand(T isLeft, T isRight) :isLeft(isLeft), isRight(isRight) {};
+
+	T getIsLeft()
+
+	{
+
+		return this->isLeft;
+
+	}
+
+	T getIsRight()
+
+	{
+
+		return this->isRight;
+
+	}
+
+	void setIsLeft(T a)
+
+	{
+
+		this->isLeft = a;
+
+	}
+
+	void setIsRight(T a)
+
+	{
+
+		this->isRight = a;
+
+	}
+
+};
+
